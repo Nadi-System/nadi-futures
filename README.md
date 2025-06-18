fb413f23afba4913bd8fd31e403f649969d463d9
Gaurav Atreya <allmanpride@gmail.com>
2025-06-17 20:30:49 -0400

    modify with sample functions

---
+++ README.md
@@ -0,0 +1,7 @@
+# Plugins for the Future version of nadi
+
+This is a plugin for [nadi-system](https://github.com/Nadi-System/nadi-system)
+
+This plugin will host the nadi functions that might be added into the future versions of nadi.
+
+compile this with `cargo build --release` and copy the dynamic libraries to a directory inside the `NADI_PLUGIN_DIRS` to load the plugin.
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,36 @@
-
 use nadi_core::nadi_plugin::nadi_plugin;
 
 #[nadi_plugin]
 mod nadi_futures {
+    use nadi_core::nadi_plugin::{env_func, network_func, node_func};
     use nadi_core::prelude::*;
+    use std::collections::HashSet;
 
-    /// The macros imported from nadi_plugin read the rust function you
-    /// write and use that as a base to write more core internally that
-    /// will be compiled into the shared libraries. This means it'll
-    /// automatically get the argument types, documentation, mutability,
-    /// etc. For more details on what they can do, refer to nadi book.
-    use nadi_core::nadi_plugin::{node_func, network_func, env_func};
-
-    /// Example Environment function for the plugin
-    ///
-    /// You can use markdown format to write detailed documentation for the
-    /// function you write. This will be availble from nadi-help.
-    #[env_func(pre = "Message: ")]
-    fn echo(message: String, pre: String) -> String {
-         format!("{}{}", pre, message)
+    /// Concat strings
+    #[env_func(sep = "")]
+    fn join(values: &[String], sep: &str) -> String {
+        values.join(sep)
     }
 
-    /// Example Node function for the plugin
+    /// all attributes from the node
     #[node_func]
-    fn node_name(node: &NodeInner) -> String {
-         node.name().to_string()
+    fn ls(node: &NodeInner) -> Vec<String> {
+        node.attr_map().keys().map(|k| k.to_string()).collect()
     }
 
-    /// Example Network function for the plugin
-    ///
-    /// You can also write docstrings for the arguments, this syntax is not
-    /// a valid rust syntax, but our macro will read those docstrings, saves
-    /// it and then removes it so that rust does not get confused. This means
-    /// You do not have to write separate documentation for functions.
-    #[network_func]
-    fn node_first_with_attr(
-        net: &Network,
-        /// Name of the attribute to search
-        attrname: String,
-    ) -> Option<String> {
-         for node in net.nodes() {
-             let node = node.lock();
-             if node.attr_dot(&attrname).is_ok() {
-                 return Some(node.name().to_string())
-             }
+    /// All attribute names present in all the nodes
+    #[network_func(ignore=vec![])]
+    fn ls(net: &Network, ignore: Vec<String>) -> Vec<String> {
+        let mut attrs = HashSet::new();
+        for node in net.nodes() {
+            node.lock()
+                .attr_map()
+                .keys()
+                .filter(|k| !ignore.contains(&k.to_string()))
+                .for_each(|k| {
+                    attrs.insert(k.to_string());
+                })
         }
-        None
+        attrs.into_iter().collect()
     }
 }
